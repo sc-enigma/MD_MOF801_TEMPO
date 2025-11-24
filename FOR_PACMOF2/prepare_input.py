a0aca929299cbe42bf8f6bbb763d33b7ad218526
sc-enigma <d.alimov.remote@gmail.com>
2025-11-24 15:55:35 +0700

    main

--- FOR_PACMOF2/prepare_input.py
+++ FOR_PACMOF2/prepare_input.py
@@ -8,7 +8,6 @@ from read_utils import read_mol2_file
 from write_utils import write_gro_file, write_mol2_file
 from write_utils import write_atoms, write_bonds, write_angles, write_dihedrals, compose_itp_files
 from pbc import calculate_lattice_vectors, select_in_box
-from mof801_utils import remove_extra_oxygens, define_mof801_atom_types, define_mof801_atom_names, check_mof801_atom_names
 from add_hydrogens import add_hydrogens
 
 # STEP 1. Read data
@@ -19,17 +18,27 @@ c     = 17.8348
 alpha = 90.00 / 180 * np.pi
 beta  = 90.00 / 180 * np.pi
 gamma = 90.00 / 180 * np.pi
-bounds_a, bounds_b, bounds_c = [0.0, 2.5], [0.0, 2.5], [0.0, 2.5]
-atoms = mol2_to_atoms(read_mol2_file('test.mol2'))
+atoms = mol2_to_atoms(read_mol2_file('source.mol2'))
 
 # STEP 2. Select atoms
 ids_selected = []
+# For 2x2x2
+'''
 for idx in range(len(atoms)):
     if atoms[idx].r[0] > 17 and atoms[idx].r[0] < 37 and atoms[idx].r[1] > 17 and atoms[idx].r[1] < 37 and atoms[idx].r[2] > 17 and atoms[idx].r[2] < 37:
         ids_selected.append(idx)
     if atoms[idx].mol2name == 'Zr' and atoms[idx].r[0] > 12 and atoms[idx].r[0] < 42 and atoms[idx].r[1] > 12 and atoms[idx].r[1] < 42 and atoms[idx].r[2] > 12 and atoms[idx].r[2] < 42:
         ids_selected.append(idx)
     if atoms[idx].mol2name == 'O3' and atoms[idx].r[0] > 12 and atoms[idx].r[0] < 42 and atoms[idx].r[1] > 12 and atoms[idx].r[1] < 42 and atoms[idx].r[2] > 12 and atoms[idx].r[2] < 42:
+        ids_selected.append(idx)
+'''
+# For 4x4x4
+for idx in range(len(atoms)):
+    if atoms[idx].r[0] > 8.0826 and atoms[idx].r[0] < 45.9174 and atoms[idx].r[1] > 8.0826 and atoms[idx].r[1] < 45.9174 and atoms[idx].r[2] > 8.0826 and atoms[idx].r[2] < 45.9174:
+        ids_selected.append(idx)
+    if atoms[idx].mol2name == 'Zr' and atoms[idx].r[0] > 3.0826 and atoms[idx].r[0] < 50.9174 and atoms[idx].r[1] > 3.0826 and atoms[idx].r[1] < 50.9174 and atoms[idx].r[2] > 3.0826 and atoms[idx].r[2] < 50.9174:
+        ids_selected.append(idx)
+    if atoms[idx].mol2name == 'O3' and atoms[idx].r[0] > 3.0826 and atoms[idx].r[0] < 50.9174 and atoms[idx].r[1] > 3.0826 and atoms[idx].r[1] < 50.9174 and atoms[idx].r[2] > 3.0826 and atoms[idx].r[2] < 50.9174:
         ids_selected.append(idx)
 
 atoms = remove_atoms(atoms, list(set(list(np.arange(len(atoms)))) ^ set(ids_selected)))
@@ -38,4 +47,4 @@ atoms = remove_atoms(atoms, list(set(list(np.arange(len(atoms)))) ^ set(ids_sele
 atoms = add_hydrogens(atoms)
 
 # STEP 4. Write data
-write_mol2_file(atoms, 'test123.mol2', a, b, c, alpha, beta, gamma)
+write_mol2_file(atoms, 'mof801_333.mol2', a, b, c, alpha, beta, gamma)
