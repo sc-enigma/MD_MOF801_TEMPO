9053c051ba00e9ba69ef38557e6eb0561c927a41
sc-enigma <d.alimov.remote@gmail.com>
2025-12-18 20:23:56 +0700

    main

--- PREPARATION/mof801_params.py
+++ PREPARATION/mof801_params.py
@@ -12,12 +12,12 @@ def get_mof801_params():
     # [atom_type] = q
     charge = {}
     charge['itcFF_Zr'] =  1.968
-    charge['itcFF_O1'] = -0.633
+    charge['itcFF_O1'] = -0.533
     charge['itcFF_O2'] = -0.902
     charge['itcFF_C1'] =  0.630
-    charge['itcFF_C2'] = -0.309
+    charge['itcFF_C2'] = -0.366
     charge['itcFF_H1'] =  0.131
-    charge['itcFF_H7'] =  0.402
+    charge['itcFF_H7'] =  0.131
     
     # [atom_type-atom_type] = [funct, r0, k]
     bond_params = {}
@@ -30,18 +30,17 @@ def get_mof801_params():
     bond_params['itcFF_C1-itcFF_C2'] = [1, 0.1514, 293928.300]
     bond_params['itcFF_C2-itcFF_C2'] = [1, 0.1324, 293928.300]
     
-    # [atom_type-atom_type-atom_type] = [funct, angle, k]
+    [atom_type-atom_type-atom_type] = [funct, angle, k]
     angle_params = {}
-    # angle_params['itcFF_O1-itcFF_Zr-itcFF_O1'] = [1,  74.400, 115.776]
+    angle_params['itcFF_O1-itcFF_Zr-itcFF_O1'] = [1,  74.400, 115.776]
     angle_params['itcFF_O2-itcFF_Zr-itcFF_O2'] = [1, 100.400, 115.776]
-    # angle_params['itcFF_O1-itcFF_Zr-itcFF_O2'] = [1, 150.500, 115.776]
+    angle_params['itcFF_O1-itcFF_Zr-itcFF_O2'] = [1, 150.500, 115.776]
     angle_params['itcFF_C1-itcFF_O1-itcFF_Zr'] = [1, 135.800, 231.637]
-    # angle_params['itcFF_O1-itcFF_C1-itcFF_O1'] = [1, 125.000,1213.360]    
-    # angle_params['itcFF_C2-itcFF_C1-itcFF_O1'] = [1, 117.300, 456.013]    
+    angle_params['itcFF_O1-itcFF_C1-itcFF_O1'] = [1, 125.000,1213.360]    
+    angle_params['itcFF_C2-itcFF_C1-itcFF_O1'] = [1, 117.300, 456.013]    
     angle_params['itcFF_C2-itcFF_C2-itcFF_H1'] = [1, 120.000, 309.616]
     angle_params['itcFF_C1-itcFF_C2-itcFF_H1'] = [1, 120.000, 309.616]
     angle_params['itcFF_C1-itcFF_C2-itcFF_C2'] = [1, 120.000, 231.637]
-    # angle_params['itcFF_H7-itcFF_O2-itcFF_Zr'] = [1, 134.8165, 460.240]
     
     # [atom_type-atom_type-atom_type-atom_type] = [funct, angle, k, n]        - periodic
     # [atom_type-atom_type-atom_type-atom_type] = [funct, c1, c2, c3, c4, c5] - fourier
